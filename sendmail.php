43b7e8bf6dbb264c61a31e9cb6eb032382142fa4
jem-cloud <84619193+jem-cloud@users.noreply.github.com>
2021-05-23 09:51:31 +0800

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,42 +1,53 @@
 <?php
-include('smtp/PHPMailerAutoload.php');
-$html='Msg';
-$alert = '';
-echo smtp_mailer('jemina.cayme.sy@gmail.com','subject',$html);
-function smtp_mailer($to,$subject, $msg){
-	$mail = new PHPMailer(); 
-	$mail->SMTPDebug  = 3;
-	$mail->IsSMTP(); 
-	$mail->SMTPAuth = true; 
-	$mail->SMTPSecure = 'tls'; 
-	$mail->Host = "smtp.gmail.com";
-	$mail->Port = 587; 
-	$mail->IsHTML(true);
-	$mail->CharSet = 'UTF-8';
-	$mail->Username = "jemina.cayme.sy@gmail.com";
-	$mail->Password = "ics2608asdfghjkl";
-	$mail->SetFrom("jemina.cayme.sy@gmail.com");
-	$mail->Subject = 'New Form Submission (Contact Us)';
-	$mail->Body = "<h3>First Name: $fname <br /> 
-			Last Name: $lname <br />
-			Phone Number: $phoneNum <br /> 
-			Email: $email <br /> 
-			Package: $package <br /> 
-			Message: $message</h3>";
-	$mail->AddAddress($to);
-	$mail->SMTPOptions=array('ssl'=>array(
-		'verify_peer'=>false,
-		'verify_peer_name'=>false,
-		'allow_self_signed'=>false
-	));
-	if(!$mail->Send()){
-		$alert = '<div class = "alert-error">
-				<span>'.$e->getMessage().'</span>
-				</div>';
-	}else{
-		$alert = '<div class = "alert-success">
-				<span>Message sent! Thank you for contacting us.</span>
-				</div>';
+	use PHPMailer\PHPMailer\PHPMailer;
+
+	require_once 'phpmailer/Exception.php';
+	require_once 'phpmailer/PHPMailer.php';
+	require_once 'phpmailer/SMTP.php';
+
+	$mail = new PHPMailer(true);
+
+	$alert = '';
+
+
+
+	if(isset($_POST['submit'])){
+		$fname = $_POST['firstname'];
+		$lname = $_POST['lastname'];
+		$phoneNum = $_POST['phoneumber'];
+		$email = $_POST['email'];
+		$package = $_POST['country'];
+		$message = $_POST['subject'];
+
+		try{
+			$mail->isSMTP();
+			$mail->Host = 'smtp.gmail.com';
+			$mail->SMTPAuth = true;
+			$mail->Username = 'jemina.cayme.sy@gmail.com';
+			$mail->Password = 'asdfghjkl\~';
+			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+			$mail->Port = '587';
+
+			$mail->setFrom('jemina.cayme.sy@gmail.com');
+			$mail->addAddress('jemina.cayme.sy@gmail.com');
+
+			$mail->isHTML(true);
+			$mail->Subject = 'Message Received (Contact Page)';
+			$mail->Body = "<h3>First Name: $fname <br /> 
+						Last Name: $lname <br />
+						Phone Number: $phoneNum <br /> 
+						Email: $email <br /> 
+						Package: $package <br /> 
+						Message: $message</h3>";
+
+			$mail->send();
+			$alert = '<div class = "alert-success">
+						<span>Message sent! Thank you for contacting us.</span>
+					</div>';
+		} catch (Exception $e){
+			$alert = '<div class = "alert-error">
+						<span>'.$e->getMessage().'</span>
+					</div>';
+		}
 	}
-}
 ?>
